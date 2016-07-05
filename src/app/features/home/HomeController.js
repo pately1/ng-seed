3cc26fe1984cb47ca0d20ba77fa237d2bc0f1a92
Yash Patel <Yash Patel>
2016-07-05 18:56:20 -0400

    refactor(catalog)

--- src/app/features/home/HomeController.js
+++ src/app/features/home/HomeController.js
@@ -26,9 +26,9 @@ angular.module('homepage', [])
   };
 
   // For Movies
-  $scope.filtText1 = '';
-  $scope.sortTab1 = '';
-  $scope.ordr1 = '';
+  this.filtText1 = '';
+  this.sortTab1 = '';
+  this.ordr1 = '';
 
   // For TV shows
   $scope.filtText2 = '';
@@ -36,43 +36,43 @@ angular.module('homepage', [])
   $scope.ordr2 = '';
 
   // For Movies
-  $scope.searchData = '';
-  $scope.search = function () {
-    $scope.filtText1 = $scope.searchData;
+  this.searchData = '';
+  this.search = function () {
+    this.filtText1 = this.searchData;
   };
 
-  $scope.genreSort = function (setTab) {
+  this.genreSort = function (setTab) {
     console.log(setTab);
     if (setTab == 1){
-      $scope.ordr1 = 'Action';
+      this.ordr1 = 'Action';
     }
     else if (setTab == 2){
       console.log('IN');
-      $scope.ordr1 = 'Adventure';
+      this.ordr1 = 'Adventure';
     }
     else if (setTab == 3){
-      $scope.ordr1 = 'Sci-Fi';
+      this.ordr1 = 'Sci-Fi';
     }
     else if (setTab == 4){
-      $scope.ordr1 = 'Thriller';
+      this.ordr1 = 'Thriller';
     }
     else if (setTab == 5){
-      $scope.ordr1 = 'Crime';
+      this.ordr1 = 'Crime';
     }
     else if (setTab == 6){
-      $scope.ordr1 = 'Drama';
+      this.ordr1 = 'Drama';
     }
     else if (setTab == 7){
-      $scope.ordr1 = 'Fantasy';
+      this.ordr1 = 'Fantasy';
     }
     else if (setTab == 8){
-      $scope.ordr1 = 'Comedy';
+      this.ordr1 = 'Comedy';
     }
     else if (setTab == 9){
-      $scope.ordr1 = 'History';
+      this.ordr1 = 'History';
     }
     else if (setTab == 10){
-      $scope.ordr1 = '';
+      this.ordr1 = '';
     }
     // $scope.tab = setTab;
     // if ($scope.tab == 1){
@@ -82,76 +82,76 @@ angular.module('homepage', [])
     //   $scope.filtText = 'series';
     // }
   };
-$scope.sortMovie = function (tab) {
+this.sortMovie = function (tab) {
   if (tab == 1){
-    $scope.sortTab1 = '-Year';
+    this.sortTab1 = '-Year';
   }
   else if (tab == 2){
     console.log('IN');
-    $scope.sortTab1 = '-imdbRating';
+    this.sortTab1 = '-imdbRating';
   }
   else if (tab == 3){
-    $scope.sortTab1 = '-imdbVotes';
+    this.sortTab1 = '-imdbVotes';
   }
   else if (tab == 4){
-    $scope.sortTab1 = '';
+    this.sortTab1 = '';
   }
 };
   // For TV Shows
 
-  $scope.genreSortTv = function (setTab) {
-    console.log(setTab);
-    if (setTab == 1){
-      $scope.ordr2 = 'Action';
-    }
-    else if (setTab == 2){
-      console.log('IN');
-      $scope.ordr2 = 'Adventure';
-    }
-    else if (setTab == 3){
-      $scope.ordr2 = 'Sci-Fi';
-    }
-    else if (setTab == 4){
-      $scope.ordr2 = 'Thriller';
-    }
-    else if (setTab == 5){
-      $scope.ordr2 = 'Crime';
-    }
-    else if (setTab == 6){
-      $scope.ordr2 = 'Drama';
-    }
-    else if (setTab == 7){
-      $scope.ordr2 = 'Fantasy';
-    }
-    else if (setTab == 8){
-      $scope.ordr2 = 'Comedy';
-    }
-    else if (setTab == 9){
-      $scope.ordr2 = 'History';
-    }
-    else if (setTab == 10){
-      $scope.ordr2 = '';
-    }
-  };
-
-  $scope.sortTv = function (tab) {
-    if (tab == 1){
-      $scope.sortTab2 = '-Year';
-    }
-    else if (tab == 2){
-      console.log('IN');
-      $scope.sortTab2 = '-imdbRating';
-    }
-    else if (tab == 3){
-      $scope.sortTab2 = '-imdbVotes';
-    }
-    else if (tab == 4){
-      $scope.sortTab2 = '';
-    }
-  };
-  $scope.searchDataTv = '';
-  $scope.searchTv = function () {
-    $scope.filtText2 = $scope.searchDataTv;
-  }
-}]);
+//   $scope.genreSortTv = function (setTab) {
+//     console.log(setTab);
+//     if (setTab == 1){
+//       $scope.ordr2 = 'Action';
+//     }
+//     else if (setTab == 2){
+//       console.log('IN');
+//       $scope.ordr2 = 'Adventure';
+//     }
+//     else if (setTab == 3){
+//       $scope.ordr2 = 'Sci-Fi';
+//     }
+//     else if (setTab == 4){
+//       $scope.ordr2 = 'Thriller';
+//     }
+//     else if (setTab == 5){
+//       $scope.ordr2 = 'Crime';
+//     }
+//     else if (setTab == 6){
+//       $scope.ordr2 = 'Drama';
+//     }
+//     else if (setTab == 7){
+//       $scope.ordr2 = 'Fantasy';
+//     }
+//     else if (setTab == 8){
+//       $scope.ordr2 = 'Comedy';
+//     }
+//     else if (setTab == 9){
+//       $scope.ordr2 = 'History';
+//     }
+//     else if (setTab == 10){
+//       $scope.ordr2 = '';
+//     }
+//   };
+//
+//   $scope.sortTv = function (tab) {
+//     if (tab == 1){
+//       $scope.sortTab2 = '-Year';
+//     }
+//     else if (tab == 2){
+//       console.log('IN');
+//       $scope.sortTab2 = '-imdbRating';
+//     }
+//     else if (tab == 3){
+//       $scope.sortTab2 = '-imdbVotes';
+//     }
+//     else if (tab == 4){
+//       $scope.sortTab2 = '';
+//     }
+//   };
+//   $scope.searchDataTv = '';
+//   $scope.searchTv = function () {
+//     $scope.filtText2 = $scope.searchDataTv;
+//   }
+ }]);
 })(angular);
